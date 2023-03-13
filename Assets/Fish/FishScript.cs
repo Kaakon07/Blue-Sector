fabb8a07136d65fb193700291c5339afcdaf7afc
Emil Aron Andresen Mathiesen <eamathie@stud.ntnu.no>
2023-03-13 12:20:08 +0100

    feat: fish system where fish can swim and move according to hunger
    optional body: fish move up when hungry. There are parameters to decide
    amount of fish and so on.
    
    Co-authored-by: Lan quynhlanp@gmail.com

---
+++ Assets/Fish/FishScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2ffa949ea8ef642b07656ccb87c7a8127b5a004547ce018be31a543672529d6d
+size 4037
---
+++ Assets/MerdSystem/FishSystemScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:22296e7f04fb6204be0f2b9fcd620d46fea7316f05508eff405b74757ddb7cc6
+size 812
