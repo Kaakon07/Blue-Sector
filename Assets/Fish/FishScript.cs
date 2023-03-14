4e2bcc8d25e7cf1ef5dd71a16aeeb04c69e66f20
Quynh-Lan Nguyen Pham <qlpham@stud.ntnu.no>
2023-03-14 11:23:39 +0100

    feat: scoring based on fish performance
    optional body: Gives a score based on fish performance in a time period.

--- Assets/Fish/FishScript.cs
+++ Assets/Fish/FishScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ffa949ea8ef642b07656ccb87c7a8127b5a004547ce018be31a543672529d6d
-size 4037
+oid sha256:f120801bb40846050b15fc228117fed882402bd0cd7d871a0caa434b9377068c
+size 4321
---
+++ Assets/FishFeeding/Score/Scoring.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:78ffccb3818a0cba069ce2a8296894e51be15f40aee706ef74dde62fa0df832e
+size 1828
