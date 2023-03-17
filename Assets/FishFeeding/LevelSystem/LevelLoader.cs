a821fee328cdb6e595ac01620b4065eb4af6a6e0
Tor Jacob Neple <torjne@stud.ntnu.no>
2023-03-17 17:32:21 +0100

    feat: add level-loader
    refs: #27 #54

---
+++ Assets/FishFeeding/LevelSystem/LevelLoader.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:803d6af75d8b53a076d3f08fc7eff094036743af2fe8bce7aca00bb2ddaa80ad
+size 2160
---
+++ Assets/FishFeeding/Scripts/Levels.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c935c2118da8149821eafc9b8d2819ed6b2e84b9fc42bbd013eb60caf49092ac
+size 729
