7a3c29ddbc4351876520732d52581e9f990d7891
Emil Aron Andresen Mathiesen <eamathie@stud.ntnu.no>
2023-03-20 11:58:59 +0100

    fix: make particle system trigger and clean project
    Remove unused assets and folders used for testing. Particles fixed by
    unchecking Play On Awake.
    
    Refs: #32

--- Assets/FishFeeding/FishSystem/FishSystemScript.cs
+++ Assets/FishFeeding/FishSystem/FishSystemScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50cb85506c5a927d59cb424ea78753fefac774bc2f506417e03c9c11e3aa0ec1
-size 3576
+oid sha256:4311df2a378300cc9c4d87e86651fa564900f6fb275f4a6a1da28b49a2ea68cd
+size 3741
--- Assets/ViewCylinderScript.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7ff1add6a0bde7fd3179a6da14d1371a59c42d1d95f763de6727569a05afb30e
-size 439
