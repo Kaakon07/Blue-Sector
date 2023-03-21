2c61d543475421a72a60caacd19c20565cdfa880
Emil Aron Andresen Mathiesen <eamathie@stud.ntnu.no>
2023-03-21 18:35:18 +0100

    chore: remove unused variables
    Refs: #35

--- Assets/FishFeeding/Fish/FishScript.cs
+++ Assets/FishFeeding/Fish/FishScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0285d3210d96ad7ef818f9988ac8d7ffc13153d5628de856c467d3e1c21917fa
-size 5402
+oid sha256:edc92c477a687d9395a2edd98143ee474c946f15efe8b093c9d82f0c783bfcd2
+size 5149
--- Assets/FishFeeding/FishSystem/FishSystemScript.cs
+++ Assets/FishFeeding/FishSystem/FishSystemScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26ead185681746816031eb8c5997b08aea152ddb45a1ee0c9e552511c2121aa6
-size 9360
+oid sha256:035ffa6292b84daa74b4e5984dfa11533780c3a303a23c1f73b184d31a4a173b
+size 9287
