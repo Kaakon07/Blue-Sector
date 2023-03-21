de95a9200dc205848127125d9179afb4eb18fec2
Quynh-Lan Nguyen Pham <qlpham@stud.ntnu.no>
2023-03-21 20:28:30 +0100

    feat: add monitors and on-screen score
    Added monitors and text on screen. The score is now shown on the screen
    after the game ends.
    
    Refs: #33

--- Assets/FishFeeding/Score/Scoring.cs
+++ Assets/FishFeeding/Score/Scoring.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8ca85023c2040f28fee3c0f16f08c7c73f7cf77f03239121e5dd83083452471
-size 2513
+oid sha256:c92c486e14fca53371b316714ccfc80191cf338d102c42dbae78eb3021562200
+size 2714
