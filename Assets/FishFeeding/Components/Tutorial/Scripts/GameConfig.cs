8df51a181371c5766fec3877480ccc6392a2fbdb
Trym Lund Flogard <trym@flogard.no>
2023-03-30 18:52:06 +0200

    feat: disable hints in realism mode
    refs: #52

---
+++ Assets/FishFeeding/Components/Tutorial/Scripts/GameConfig.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ed1ef646d0f719f6b1af5f798486b96803875356c0b38e91fcc96950d0ed045d
+size 1264
--- Assets/FishFeeding/Components/Tutorial/Scripts/Popup.cs
+++ Assets/FishFeeding/Components/Tutorial/Scripts/Popup.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b92f00d0c3a3eb055764162801c4e17ce1576e8db91379f4526fd3e25d985af
-size 3818
+oid sha256:304df878d3bad33bd1dd0ec0a4905ff4fd012d534dd7e00d772838b765a4ccd8
+size 3308
