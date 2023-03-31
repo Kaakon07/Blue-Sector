2e31c862057ecda32d14aa68f26ee960dab2f040
Quynh-Lan Nguyen Pham <quynhlanp@gmail.com>
2023-03-31 13:29:51 +0200

    feat: combined HUD with merdmonitor
    Added screen visualization with the camera and fishsystem. Food wastage
    and dead fish still needs to be correctly shown.
    
    Refs: #33

--- Assets/FishFeeding/FishSystem/FishSystemScript.cs
+++ Assets/FishFeeding/FishSystem/FishSystemScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ae6bda4951813e747b8d56ddcdb6aa450d3f1cb16a5360bd17b36c22b12302d
-size 9113
+oid sha256:1af8f0ae02cc9fa44281e33e65d9af8ee3ffc09a4d8c99b26e72685587576002
+size 9117
--- Assets/FishFeeding/Score/Scoring.cs
+++ Assets/FishFeeding/Score/Scoring.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:770aa650ebe55346c31ec785e9283364b362464f49a9f42eca1a8c31e34776d7
-size 4382
+oid sha256:f3757e848ea149b1d8d9c38a471c970cf246f44c0cf047f415ba3cf274303c9c
+size 4720
