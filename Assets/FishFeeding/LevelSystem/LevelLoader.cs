add84ffa610fcd617c27ed9e9bd30aa9aeb5680c
Tor Jacob Neple <62819835+torjacob@users.noreply.github.com>
2023-03-31 15:26:19 +0200

    refactor: load modes instead of levels

--- Assets/FishFeeding/LevelSystem/LevelLoader.cs
+++ Assets/FishFeeding/LevelSystem/LevelLoader.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:803d6af75d8b53a076d3f08fc7eff094036743af2fe8bce7aca00bb2ddaa80ad
-size 2160
+oid sha256:b692fe9dee9385dc8c25c66676e94aa4811f8443e624e25fdf90032c0a296ca2
+size 2024
