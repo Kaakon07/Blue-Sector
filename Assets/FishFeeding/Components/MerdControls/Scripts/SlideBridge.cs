b355beac4c475b426ac2a94c33f6d4801c6c0a37
Tor Jacob Neple <62819835+torjacob@users.noreply.github.com>
2023-03-31 15:22:55 +0200

    feat: implement fish feed slider

---
+++ Assets/FishFeeding/Components/MerdControls/Scripts/SlideBridge.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b15c6c4e8d9ee611d89cdcd836484a847c6c6d0f305ca36fde85280c95244081
+size 1291
--- Assets/FishFeeding/FishSystem/FishSystemScript.cs
+++ Assets/FishFeeding/FishSystem/FishSystemScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1af8f0ae02cc9fa44281e33e65d9af8ee3ffc09a4d8c99b26e72685587576002
-size 9117
+oid sha256:b6b6ca767ccaeefcff4a8a00876d6b9f086a040ebed227485bd9640a368f81be
+size 8779
