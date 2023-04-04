75f2ceda322fb6434225b39dd10cbcbb759bb30a
Quynh-Lan Nguyen Pham <quynhlanp@gmail.com>
2023-04-04 13:44:21 +0200

    chore: made prefabs and improved file structure
    Added the canvas prefab containing the HUD to the MerdCameraHost prefab.
    Moved prefabs related to scoring to the Scoring folder.
    
    Refs: #68

--- Assets/FishFeeding/Score/Scoring.cs
+++ Assets/FishFeeding/Score/Scoring.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5823ff9f8aef4e0ab100c267871e19ce8b7183279b5076e8ee22121e0b0fba6a
-size 4725
+oid sha256:5234e0447cf3f913438430c30fcc1fc2801284d176136e5ea33d7d920acb230b
+size 4985
