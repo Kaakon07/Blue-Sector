8165242a807df2877758a6e89ab590b4044741da
Emil Aron Andresen Mathiesen <eamathie@stud.ntnu.no>
2023-04-10 09:52:45 +0200

    fix: shift position of fish sprite and improve swimming
    less aggressive pitching when swimming up or down, and shift the
    position of the sprite so it lines up with the 3D model, making the
    transition more seamless.
    
    Refs: #65

--- Assets/FishFeeding/Fish/FishScript.cs
+++ Assets/FishFeeding/Fish/FishScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c120c77df47d1d0d47975eb7b79b4bfdf5b9e95e566a14844224d9949141c208
-size 7077
+oid sha256:3bcbe5c3a74850794c9e2499ac691a1620559042c33f8264b86d5cfe90d36b0e
+size 7062
--- Assets/FishFeeding/FishSystem/FishSystemScript.cs
+++ Assets/FishFeeding/FishSystem/FishSystemScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:070d1bf70e224a0115bf9449f6ad780f48ec111b845037bdcf8d4b90a7a10bb0
-size 9153
+oid sha256:64939e99cb3836bfa02a7265f33fb0feb66c6c65939426eac1a2264ad6c19eea
+size 9690
