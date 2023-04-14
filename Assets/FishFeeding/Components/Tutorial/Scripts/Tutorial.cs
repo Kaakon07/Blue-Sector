144013f49cb535a3a3122727fa2ef77dcb77cc81
Quynh-Lan Nguyen Pham <quynhlanp@gmail.com>
2023-04-14 11:28:54 +0200

    Merge changes from blue-sector2/master (demo 1)

--- Assets/FishFeeding/Components/Tutorial/Scripts/Tutorial.cs
+++ Assets/FishFeeding/Components/Tutorial/Scripts/Tutorial.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a212b780e514889a3fc06b04f0cbd42f3e38692e41eb23ead7d3dd5fefe52744
-size 2919
+oid sha256:eeacb85350c85828741915f41a7c7d02038fff4b0ea457ea2e3650a87032867f
+size 3001
--- Assets/FishFeeding/Components/Tutorial/Scripts/TutorialEntry.cs
+++ Assets/FishFeeding/Components/Tutorial/Scripts/TutorialEntry.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e61262fb279c933f4ec6a71bac69f9f5c2de101d462c18ffa12353661302890d
-size 1644
+oid sha256:fe08172354c1e3cf431672dfc562baaa1b280d2aeedf0e39c9de8f5eeee4a9e1
+size 1684
