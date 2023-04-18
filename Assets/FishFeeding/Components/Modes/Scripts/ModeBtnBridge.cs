64d2c290c498d0dc86a78dbfd4b5e86406fb53d4
Tor Jacob Neple <62819835+torjacob@users.noreply.github.com>
2023-04-19 01:29:44 +0200

    feat: add bridge script for mode button
    Refs: #66

---
+++ Assets/FishFeeding/Components/Modes/Scripts/ModeBtnBridge.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4e856961087c1f1eaa2c825c29cc74daac0ba9b8f5d53474b80ad82dea2c7c8c
+size 847
--- Assets/FishFeeding/Scripts/Game.cs
+++ Assets/FishFeeding/Scripts/Game.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:881fa19a0c995a19dfeec461346d96d6e64b6d6fc8717eb24ec476d0596c4d7a
-size 5248
+oid sha256:8762672932fe12eb660f97bff7ebd7fcc7809a7ea9ed3ebae8b8f3a58f5711b4
+size 5255
