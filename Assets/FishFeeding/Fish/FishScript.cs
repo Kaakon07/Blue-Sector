439b0e95a02e6ac56e04aa0bcde83d64cec1ce8b
Trym Lund Flogard <trym@flogard.no>
2023-04-19 19:56:05 +0200

    feat: improve fish update-loop perf.

--- Assets/FishFeeding/Fish/FishScript.cs
+++ Assets/FishFeeding/Fish/FishScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff279c5a0ca015b48142c3fc030a0680773ab516096586e49b25a4566777187e
-size 7558
+oid sha256:da067c0485c8824e07a185c6561d93cd0ed4abeeb84a729f412a570b85f9f6e3
+size 7355
