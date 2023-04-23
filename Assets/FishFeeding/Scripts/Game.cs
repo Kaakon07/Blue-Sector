13ea24828148ae8de91f609569ea8fdd9517d37a
Quynh-Lan Nguyen Pham <quynhlanp@gmail.com>
2023-04-23 15:18:44 +0200

    feat: foodwastebar is a different color based on the slider value

--- Assets/FishFeeding/Scripts/Game.cs
+++ Assets/FishFeeding/Scripts/Game.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8b187b244778f1e41c8847a4eb10d5a2708f180cdc2df4705a96b176bb93ab5
-size 5685
+oid sha256:e9f68493539610f15186bd6b822ec9d2bf33fde6ad45d44a6ef489c9eab84d71
+size 6633
