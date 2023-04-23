0cc237f7c0ebc607f9f5eff50523e28f0bae83db
Trym Lund Flogard <trym@flogard.no>
2023-04-23 16:15:51 +0200

    fix: start game outside of box collider

--- Assets/FishFeeding/Scripts/Game.cs
+++ Assets/FishFeeding/Scripts/Game.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfa20be8b3beeafed302cd74b235bc1dfeda0971f214da489af4c56607414f4f
-size 5524
+oid sha256:c8b187b244778f1e41c8847a4eb10d5a2708f180cdc2df4705a96b176bb93ab5
+size 5685
--- Assets/FishFeeding/Scripts/StartGameToolTip.cs
+++ Assets/FishFeeding/Scripts/StartGameToolTip.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2a1793176be9e0d172637fd89638684bbe47cdac3dc9fe4a33028f4df6bb99b
-size 1023
+oid sha256:540569bdaa4467adc537826cfbda24173f0ce73dcba0c01e4c45ae1678dd1368
+size 1020
