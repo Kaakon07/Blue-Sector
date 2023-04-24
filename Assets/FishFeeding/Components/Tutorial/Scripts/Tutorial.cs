99d1faba71410fe05ede65a56dfe8abad076b87a
Trym Lund Flogard <trym@flogard.no>
2023-04-24 14:53:26 +0200

    fix: tutorial dismissal

--- Assets/FishFeeding/Components/Tutorial/Scripts/Tutorial.cs
+++ Assets/FishFeeding/Components/Tutorial/Scripts/Tutorial.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a87e2e6530badbb9cbbd20f0394ec4f31f80c552792e71aeb3d69f9f6607f5e
-size 3533
+oid sha256:c32047e98a7c75f84b8dfd7590538685bdeffe381be0ece9cef5e191323312c3
+size 3527
